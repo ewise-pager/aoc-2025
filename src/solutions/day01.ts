8e426fc217d4cd01bb2ed24ae88a1180c787ee9d
Evan Wise <evan_wise@onlifehealth.com>
2025-12-01 09:18:15 -0600

    add example answers, commit currently incorrect part 2 (works on example not on input)

--- src/solutions/day01.ts
+++ src/solutions/day01.ts
@@ -1,6 +1,8 @@
 import { Solution } from '../lib/Solution.ts';
 export class Day01 extends Solution {
   turns: number[] = [];
+  exampleAnswer1 = '3';
+  exampleAnswer2 = '6';
 
   async parseInput(): Promise<void> {
     for (const line of this.input.trim().split('\n')) {
@@ -25,7 +27,21 @@ export class Day01 extends Solution {
     return zeroCount.toString();
   }
 
+  // Currently wrong
   async runPart2(): Promise<string | null> {
-    return null;
+    let dial = 50;
+    let zeroCount = 0;
+    for (const turn of this.turns) {
+      const complement = turn > 0 ? 100 - dial : dial;
+      if (Math.abs(turn) >= dial) {
+        zeroCount += Math.floor((Math.abs(turn) - complement) / 100) + 1;
+      }
+      dial += turn;
+      dial = dial % 100;
+      if (dial < 0) {
+        dial += 100;
+      }
+    }
+    return zeroCount.toString();
   }
 }
