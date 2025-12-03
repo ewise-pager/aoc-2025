1c22157d5e0cd55771521348ef5bbd0c3450c12b
Evan Wise <evan_wise@onlifehealth.com>
2025-12-03 08:33:25 -0600

    run formatter

--- src/solutions/day02.ts
+++ src/solutions/day02.ts
@@ -1,17 +1,17 @@
 import { Solution } from '../lib/Solution.ts';
 
 function isNumberRepeating(number: number): boolean {
-    const numberString = number.toString();
-    if (numberString.length % 2 !== 0) {
-        return false;
+  const numberString = number.toString();
+  if (numberString.length % 2 !== 0) {
+    return false;
+  }
+  let halfLength = numberString.length / 2;
+  for (let i = 0; i < halfLength; i++) {
+    if (numberString[i] !== numberString[i + halfLength]) {
+      return false;
     }
-    let halfLength = numberString.length / 2;
-    for (let i = 0; i < halfLength; i++) {
-        if (numberString[i] !== numberString[i + halfLength]) {
-            return false;
-        }
-    }
-    return true;
+  }
+  return true;
 }
 
 export class Day02 extends Solution {
@@ -19,19 +19,22 @@ export class Day02 extends Solution {
   ranges: number[][] = [];
 
   async parseInput(): Promise<void> {
-    this.ranges = this.input.trim().split(',').map((range) => {
-      return range.split('-').map(Number);
-    });
+    this.ranges = this.input
+      .trim()
+      .split(',')
+      .map((range) => {
+        return range.split('-').map(Number);
+      });
   }
 
   async runPart1(): Promise<string | null> {
     let sum = 0;
     for (const [start, end] of this.ranges) {
-        for (let i = start; i <= end; i++) {
-            if (isNumberRepeating(i)) {
-                sum += i;
-            }
+      for (let i = start; i <= end; i++) {
+        if (isNumberRepeating(i)) {
+          sum += i;
         }
+      }
     }
     return sum.toString();
   }
--- src/solutions/index.ts
+++ src/solutions/index.ts
@@ -1,6 +1,7 @@
 import { Solution } from '../lib/Solution.ts';
 import { Day01 } from './day01.ts';
 import { Day02 } from './day02.ts';
+import { Day03 } from './day03.ts';
 
 // Add new day solutions to this array
-export const solutions: Solution[] = [new Day01(), new Day02()];
+export const solutions: Solution[] = [new Day01(), new Day02(), new Day03()];
